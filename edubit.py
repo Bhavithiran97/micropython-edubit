a2a1c01b83bb734108fd92765e25706ec2e16e78
bhavithiran97 <bhavithiran97@gmail.com>
2022-11-24 12:05:45 +0800

    Update edubit.py

--- edubit.py
+++ edubit.py
@@ -19,20 +19,22 @@ REG_ADD_PWR_STATE = 12
 REG_ADD_LB_STATE = 13
 REG_ADD_OV_STATE = 14
 
-S1 = REG_ADD_SERVO_1
-S2 = REG_ADD_SERVO_2
-S3 = REG_ADD_SERVO_3
+Servo_S1 = REG_ADD_SERVO_1
+Servo_S2 = REG_ADD_SERVO_2
+Servo_S3 = REG_ADD_SERVO_3
+Servo_All = 1000
 
-M1 = 0
-M2 = 1
-All = 1000
+Motor_M1 = 0
+Motor_M2 = 1
+Motor_All = 1000
 
-Forward = 0
-Backward = 1
+Direction_Forward = 0
+Direction_Backward = 1
 
-Red = pin14
-Yellow = pin15
-Green = pin16
+LED_Red = pin14
+LED_Yellow = pin15
+LED_Green = pin16
+LED_All = 1000
 
 SOUND_BIT_PIN = pin1
 POTENTIO_BIT_PIN = pin2
@@ -63,7 +65,7 @@ def i2cWrite(register,data):
     i2c.write(I2C_ADDRESS,buffer)
 
 
-def init():
+def power_monitor():
     global flag
     global oldPowerState
 
@@ -74,11 +76,11 @@ def init():
 
     if is_power_on():
         if oldPowerState == False:
-            brake_motor(M1)
-            brake_motor(M2)
-            disable_servo(S1)
-            disable_servo(S2)
-            disable_servo(S3)
+            brake_motor(Motor_M1)
+            brake_motor(Motor_M2)
+            disable_servo(Servo_S1)
+            disable_servo(Servo_S2)
+            disable_servo(Servo_S3)
             reset()
         oldPowerState = True
     else:
@@ -86,15 +88,15 @@ def init():
     sleep_ms(200)
 
 def brake_motor(motorChannel):
-    if motorChannel == M1:
+    if motorChannel == Motor_M1:
         i2cWrite(REG_ADD_M1A, 0)
         i2cWrite(REG_ADD_M1B, 0)
 
-    elif motorChannel == M2:
+    elif motorChannel == Motor_M2:
         i2cWrite(REG_ADD_M2A, 0)
         i2cWrite(REG_ADD_M2B, 0)
 
-    elif motorChannel == All:
+    elif motorChannel == Motor_All:
         i2cWrite(REG_ADD_M1A, 0)
         i2cWrite(REG_ADD_M1B, 0)
         i2cWrite(REG_ADD_M2A, 0)
@@ -102,22 +104,22 @@ def brake_motor(motorChannel):
 
 def run_motor(motorChannel,direction,speed):
     speed = limit(speed,0,255)
-    if motorChannel == M1:
-        if direction == Forward:
+    if motorChannel == Motor_M1:
+        if direction == Direction_Forward:
             i2cWrite(REG_ADD_M1A,speed)
             i2cWrite(REG_ADD_M1B,0)
         else:
             i2cWrite(REG_ADD_M1A,0)
             i2cWrite(REG_ADD_M1B,speed)
-    elif motorChannel == M2:
-        if direction == Forward:
+    elif motorChannel == Motor_M2:
+        if direction == Direction_Forward:
             i2cWrite(REG_ADD_M2A,speed)
             i2cWrite(REG_ADD_M2B,0)
         else:
             i2cWrite(REG_ADD_M2A,0)
             i2cWrite(REG_ADD_M2B,speed)
-    elif motorChannel == All:
-        if direction == Forward:
+    elif motorChannel == Motor_All:
+        if direction == Direction_Forward:
             i2cWrite(REG_ADD_M1A,speed)
             i2cWrite(REG_ADD_M1B,0)
             i2cWrite(REG_ADD_M2A,speed)
@@ -129,10 +131,10 @@ def run_motor(motorChannel,direction,speed):
             i2cWrite(REG_ADD_M2B,speed)
 
 def disable_servo(servo):
-    if servo == All:
-        i2cWrite(S1,0)
-        i2cWrite(S2,0)
-        i2cWrite(S3,0)
+    if servo == Servo_All:
+        i2cWrite(Servo_S1,0)
+        i2cWrite(Servo_S2,0)
+        i2cWrite(Servo_S3,0)
     else:
         i2cWrite(servo,0)
 
@@ -140,10 +142,10 @@ def sets_servo_position(servo,position):
     position = limit(position,0,180)
 
     pulseWidth = int(position * 20 / 18 + 50)
-    if servo == All:
-        i2cWrite(S1,pulseWidth)
-        i2cWrite(S2,pulseWidth)
-        i2cWrite(S3,pulseWidth)
+    if servo == Servo_All:
+        i2cWrite(Servo_S1,pulseWidth)
+        i2cWrite(Servo_S2,pulseWidth)
+        i2cWrite(Servo_S3,pulseWidth)
     else:
         i2cWrite(servo,pulseWidth)
 
@@ -169,16 +171,16 @@ def read_Vin():
     return i2cRead(REG_ADD_VIN) / 10
 
 def set_led(color,state):
-    if color == Red:
-        Red.write_digital(state)
-    if color == Yellow:
-        Yellow.write_digital(state)
-    if color == Green:
-        Green.write_digital(state)
-    if color == All:
-        Red.write_digital(state)
-        Yellow.write_digital(state)
-        Green.write_digital(state)
+    if color == LED_Red:
+        LED_Red.write_digital(state)
+    if color == LED_Yellow:
+        LED_Yellow.write_digital(state)
+    if color == LED_Green:
+        LED_Green.write_digital(state)
+    if color == LED_All:
+        LED_Red.write_digital(state)
+        LED_Yellow.write_digital(state)
+        LED_Green.write_digital(state)
 
 def read_sound_sensor():
     return SOUND_BIT_PIN.read_analog()
